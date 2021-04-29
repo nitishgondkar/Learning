1b0642de38b8052be9cc9f168d83c60fe7929afc
nitishgondkar <nitish.gondkar@gmail.com>
2021-04-29 19:47:26 +0530

    Learning Course : https://www.udemy.com/course/microservices-with-spring-boot-and-spring-cloud/

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,12 +5,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
 
 @RestController
 public class UserResource {
@@ -20,26 +18,36 @@ public class UserResource {
 	
 	//GET /users
 	//retrieveAllUsers
+	//http://localhost:8080/users
 	@GetMapping("/users")
 	public List<User> retrieveAllUsers(){
 		return service.findAll();
 	}
 	
 	//retrieveUser(int id)
+	//URL : http://localhost:8080/users/1
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
 		if(user == null) {
-			throw new UserNotFoundException("id-"+id); 
+			throw new UserNotFoundException("id-"+ id);
 		}
 		return user;
+	}
+
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = service.deleteById(id);
+		if(user == null) {
+			throw new UserNotFoundException("id-"+ id);
+		}
 	}
 	
 	//CREATED
 	//input - Details of User
 	//output - CREATED & Return the created URI
 	@PostMapping("/users")
-	public ResponseEntity<Object> createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		User savedUser = service.save(user);
 		
 		//CREATED
@@ -52,4 +60,6 @@ public class UserResource {
 		
 		return ResponseEntity.created(location).build();
 	}
+
+
 }
