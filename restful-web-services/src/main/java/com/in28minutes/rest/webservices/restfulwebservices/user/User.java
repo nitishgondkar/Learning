8016f0256fcd8aa9c4efbfa7d155323c8edb2891
nitishgondkar <nitish.gondkar@gmail.com>
2021-05-01 19:12:39 +0530

    Learning Course : https://www.udemy.com/course/microservices-with-spring-boot-and-spring-cloud/
    Completion of SWAGGER API Documentation
    http://localhost:8080/v2/api-docs

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,17 +1,24 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All details about the user") 	//Part of SWAGGER Documentation about user class in http://localhost:8080/v2/api-docs
 public class User {
 	
 	private Integer id;
 
+	@ApiModelProperty(notes = "Name should have at least 2 characters") //Part of SWAGGER Documentation about user class in http://localhost:8080/v2/api-docs
 	@Size(min = 2, message = "Name should have at least 2 characters")
 	private String name;
 
+	@ApiModelProperty(notes = "Birth Date should be in the past") //Part of SWAGGER Documentation about user class in http://localhost:8080/v2/api-docs
 	@Past
 	private Date birthDate;
 
